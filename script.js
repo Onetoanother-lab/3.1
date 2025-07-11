01d7f10531991f0a4ff2d1b119d75480b464eb3e
Onetoanother-lab <i2369096020@gmail.com>
2025-07-11 16:20:48 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-    function setMode(mode) {
-      if (mode === 'dark') {
-        document.body.classList.add('dark');
-      } else {
-        document.body.classList.remove('dark');
-      }
-        localStorage.setItem('theme', mode);
-    }
